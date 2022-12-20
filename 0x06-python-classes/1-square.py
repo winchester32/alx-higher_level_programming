c2946021525329c390d552025185eabcfe4719e0
winchester32 <wnmburuk@gmail.com>
2022-12-20 13:38:03 -0800

    square 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """new square.
+        Args:
+        size (int): size of a new square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defime a square """
+
+class Square:
+    """ a square"""
+    def __init__(self, size=0):
+        """ new square
+        args
