8b0609433d5405fc85910f773b921df5bf5e2484
winchester32 <wnmburuk@gmail.com>
2023-01-10 12:53:12 -0800

    S

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -18,4 +18,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """returns str"""
-        return "[square] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
