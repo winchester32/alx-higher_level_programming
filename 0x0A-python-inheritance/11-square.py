e8ff6147722f40bedc2e699360c5b1a0ccf6034a
winchester32 <wnmburuk@gmail.com>
2023-01-10 12:47:07 -0800

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """initializes a new square"""
         self.integer_validator("size", size)
-         self.__size = size
+        self.__size = size
         super().__init__(self.__size, self.__size)
 
     def area(self):
