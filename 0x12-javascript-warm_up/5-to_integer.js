bf075425e554d8e076f78e9d90e6a40dd4465f4c
winchester32 <wnmburuk@gmail.com>
2023-03-13 17:27:32 -0700

    js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num) === true) {
   console.log('Not a number');
 } else {
-  console.log('My number:' + num);
+  console.log('My number: ' + num);
 }
